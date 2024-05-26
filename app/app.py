f34d139af5e883de98186e3464521698db0f7912
dodopontocom <rodolfotiago@gmail.com>
2024-05-25 23:52:15 -0300

    actions II

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ import boto3
 region = 'sa-east-1'
 sqs = boto3.client('sqs', region_name=region)
 queue_url = 'https://sqs.sa-east-1.amazonaws.com/654654328943/q2'
-message_body = '{"nome":"Rodolfo","idade":22}'
+message_body = '{"nome":"Thais","idade":22}'
 response = sqs.send_message(QueueUrl=queue_url, MessageBody=message_body)
 print(f'Mensagem enviada com sucesso! ID da mensagem: {response["MessageId"]}')
