cc7ab563369f7168b15db0bcc1677cf64dc51ce0
dodopontocom <rodolfotiago@gmail.com>
2024-05-27 10:21:02 -0300

    new approach

--- app/app.py
+++ app/app.py
@@ -1,8 +1,13 @@
-import boto3
+import boto3, os
 
-region = 'sa-east-1'
+region = os.environ['TF_VAR_region']
+account_id = os.environ['aws_account_id']
+queue_name = os.environ['TF_VAR_sqs_queue_name']
+queue_url = 'https://sqs.'+ region +'.amazonaws.com/' + account_id + '/' + queue_name
+
 sqs = boto3.client('sqs', region_name=region)
-queue_url = 'https://sqs.sa-east-1.amazonaws.com/654654328943/q2'
+
 message_body = '{"nome":"Thais","idade":22}'
+
 response = sqs.send_message(QueueUrl=queue_url, MessageBody=message_body)
 print(f'Mensagem enviada com sucesso! ID da mensagem: {response["MessageId"]}')
