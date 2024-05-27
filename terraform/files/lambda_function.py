8785d5d63b44da720bb57c441a0afb5a2a9b4b42
dodopontocom <rodolfotiago@gmail.com>
2024-05-27 15:08:42 -0300

    shut down [terraform-destroy][deploy-app]

--- terraform/files/lambda_function.py
+++ terraform/files/lambda_function.py
@@ -1,25 +1,14 @@
-import json, telegram, os
-
-bot_token = os.environ['TELEGRAM_TOKEN']
-chat_id = os.environ['TELEGRAM_CHAT_ID']
-bot = telegram.Bot(token=bot_token)
+import json
 
 def lambda_handler(event, context):
-    for message in event['Records']:
-        process_message(message)
-    print("done")
-
-def process_message(message):
-    try:
-        print(f"Processed message {message['body']}")
-
-        # TODO: Do interesting work based on the new message
-        bot.send_message(chat_id=chat_id, text=f"Processamento bem-sucedido: {message}")
-
-    except Exception as err:
-        print("An error occurred")
-        raise err
-
+    for record in event['Records']:
+        body = record['body']
+        print(f"Mensagem recebida: {body}")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Messages processed com sucesso!')
+    }
 
 # def lambda_handler(event, context):
 #     for record in event['Records']:
