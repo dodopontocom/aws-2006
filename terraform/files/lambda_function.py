aa73b60a424a32db7f7401a54bfbf032725fb6f6
dodopontocom <rodolfotiago@gmail.com>
2024-05-27 12:54:25 -0300

    [terraform-apply][deploy-app] improving message python lambda

--- terraform/files/lambda_function.py
+++ terraform/files/lambda_function.py
@@ -1,12 +1,12 @@
-def lambda_handler(event, context):
-    for message in event['Records']:
-        process_message(message)
-    print("done")
+import json
 
-def process_message(message):
-    try:
-        print(f"Processed message {message['body']}")
-        # TODO: Do interesting work based on the new message
-    except Exception as err:
-        print("An error occurred")
-        raise err
+def lambda_handler(event, context):
+    for record in event['Records']:
+        # Process each message
+        body = record['body']
+        print(f"Received message: {body}")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Messages processed successfully!')
+    }
