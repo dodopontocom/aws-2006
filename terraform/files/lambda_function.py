be3ea1dcb8668069353404bad70bf62094725525
dodopontocom <rodolfotiago@gmail.com>
2024-05-27 14:56:19 -0300

    [terraform-apply][deploy-app] another try

--- terraform/files/lambda_function.py
+++ terraform/files/lambda_function.py
@@ -1,20 +1,40 @@
-import json, telegram, os, asyncio
+import json, telegram, os
+
+bot_token = os.environ['TELEGRAM_TOKEN']
+chat_id = os.environ['TELEGRAM_CHAT_ID']
+bot = telegram.Bot(token=bot_token)
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-        body = record['body']
-        print(f"Mensagem recebida: {body}")
+    for message in event['Records']:
+        process_message(message)
+    print("done")
+
+def process_message(message):
+    try:
+        print(f"Processed message {message['body']}")
+
+        # TODO: Do interesting work based on the new message
+        bot.send_message(chat_id=chat_id, text=f"Processamento bem-sucedido: {message}")
+
+    except Exception as err:
+        print("An error occurred")
+        raise err
+
+# def lambda_handler(event, context):
+#     for record in event['Records']:
+#         body = record['body']
+#         print(f"Mensagem recebida: {body}")
     
-    bot_token = os.environ['TELEGRAM_TOKEN']
-    chat_id = os.environ['TELEGRAM_CHAT_ID']
-    bot = telegram.Bot(token=bot_token)
+#     bot_token = os.environ['TELEGRAM_TOKEN']
+#     chat_id = os.environ['TELEGRAM_CHAT_ID']
+#     bot = telegram.Bot(token=bot_token)
 
-    asyncio.run(bot.send_message(chat_id=chat_id, text=f"Processamento bem-sucedido: {body}"))
+#     bot.send_message(chat_id=chat_id, text=f"Processamento bem-sucedido: {body}")
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Messages processed com sucesso!')
-    }
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Messages processed com sucesso!')
+#     }
 
 #
 # def lambda_handler(event, context):
