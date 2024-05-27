e1eeb81d474ca704176eb93c8ace6216b936376d
dodopontocom <rodolfotiago@gmail.com>
2024-05-27 13:11:52 -0300

    save for tracking

--- terraform/files/lambda_function.py
+++ terraform/files/lambda_function.py
@@ -10,3 +10,16 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Messages processed com sucesso!')
     }
+
+# def lambda_handler(event, context):
+#     for message in event['Records']:
+#         process_message(message)
+#     print("done")
+
+# def process_message(message):
+#     try:
+#         print(f"Processed message {message['body']}")
+#         # TODO: Do interesting work based on the new message
+#     except Exception as err:
+#         print("An error occurred")
+#         raise err
